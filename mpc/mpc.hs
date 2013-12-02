054754c4b85348287277dfb8b7a419c8ca796ec4
joneshf <jones3.hardy@gmail.com>
2013-12-02 10:00:50 -0800

    Started on mpc.

--- mpc/mpc.hs
+++ mpc/mpc.hs
@@ -1,1 +1,22 @@
+module Deter where
+
+-- A parser takes a String and returns a list of `(a, String)` 2-tuples.
+-- If the list is empty, the parser failed.
+-- Should this be implemented as some other ADT?
 type Parser a = String -> [(a, String)]
+
+-- Primitive parsers.
+-- These are defined this way to follow closer the type signature.
+result :: a -> Parser a
+result v = \inp -> [(v, inp)]
+
+zero :: Parser a
+zero = \inp -> []
+
+item :: Parser Char
+item = \inp -> case inp of
+    [] -> []
+    x:xs -> [(x, xs)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+p `bind` f = \inp -> concat [f v inp' | (v, inp') <- p inp]
