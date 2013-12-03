b9364dcbe0d1ad8ad63ae4d0e131943b248cd982
joneshf <jones3.hardy@gmail.com>
2013-12-03 05:49:34 -0800

    Fixed it up to use haskell monads.

--- mpc/mpc.hs
+++ mpc/mpc.hs
@@ -5,42 +5,38 @@ import Prelude hiding (seq)
 -- A parser takes a String and returns a list of `(a, String)` 2-tuples.
 -- If the list is empty, the parser failed.
 -- Should this be implemented as some other ADT?
-type Parser a = String -> [(a, String)]
+newtype Parser a = Parser (String -> [(a, String)])
 
+-- Need a way to deconstruct parsers.
+parse :: Parser a -> String -> [(a, String)]
+parse (Parser p) = p
 
--- Cant actually do this yet.
---instance Monad Parser where
---    return = result
---    (>>=) = bind
+instance Monad Parser where
+    return v = Parser $ \inp -> [(v, inp)]
+    p >>= f  = Parser $ \inp ->
+        concat [parse (f v) inp' | (v, inp') <- parse p inp]
 
 -- Primitive parsers.
 -- These are defined this way to follow closer the type signature.
-result :: a -> Parser a
-result v = \inp -> [(v, inp)]
 
 zero :: Parser a
-zero = \inp -> []
+zero = Parser $ const []
 
 item :: Parser Char
-item = \inp -> case inp of
+item = Parser $ \inp -> case inp of
     [] -> []
     x:xs -> [(x, xs)]
 
--- Our sequencing operator.
--- Easier to work with than seq.
-bind :: Parser a -> (a -> Parser b) -> Parser b
-p `bind` f = \inp -> concat [f v inp' | (v, inp') <- p inp]
-
 -- In case we wanted seq anyway.
 seq :: Parser a -> Parser b -> Parser (a, b)
-p `seq` q = p `bind` \x ->
-            q `bind` \y ->
-            result (x, y)
+p `seq` q = p >>= \x ->
+            q >>= \y ->
+            return (x, y)
 
 -- Combinator to test a specific character.
 sat :: (Char -> Bool) -> Parser Char
-sat p = item `bind` \x ->
-    if p x then result x else zero
+sat p = item >>= \x ->
+    if p x then return x else zero
 
 -- Actual parsers
 
@@ -58,7 +54,7 @@ upper = sat (\c -> 'A' <= c && c <= 'Z')
 
 -- Choice operator
 plus :: Parser a -> Parser a -> Parser a
-p `plus` q = \inp -> p inp ++ q inp
+p `plus` q = Parser $ \inp -> parse p inp ++ parse q inp
 
 -- More parsers
 
@@ -69,8 +65,8 @@ alphaNum :: Parser Char
 alphaNum = letter `plus` digit
 
 word :: Parser String
-word = nonEmpty `plus` result ""
+word = nonEmpty `plus` return ""
     where
-        nonEmpty = letter `bind` \x  ->
-                   word   `bind` \xs ->
-                   result (x:xs)
+        nonEmpty = letter >>= \x  ->
+                   word   >>= \xs ->
+                   return (x:xs)
