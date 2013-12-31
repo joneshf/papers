25c3d30fbcf2c50a01420131d7c2c5c6a51527b4
joneshf <jones3.hardy@gmail.com>
2013-12-31 10:32:22 -0800

    Cleanup.

--- Clowns_to_the_Left_Jokers_to_the_Right_Dissecting_Data_Structures/Expressions.hs
+++ Clowns_to_the_Left_Jokers_to_the_Right_Dissecting_Data_Structures/Expressions.hs
@@ -10,10 +10,10 @@ eval :: Expr -> Int
 eval e = load e []
 
 load :: Expr -> Stack -> Int
-load (Val i)    stack  = unload i stack
-load (Add e1 e2) stack = load e1 (Left e2:stack)
+load (Val i)     stack = unload i stack
+load (Add e1 e2) stack = load e1 (Left e2 : stack)
 
 unload :: Int -> Stack -> Int
-unload v [] = v
-unload v (Left e:stack) = load e (Right v:stack)
-unload v (Right e:stack) = unload (v + e) stack
+unload v  []                 = v
+unload v  (Left  e  : stack) = load e (Right v : stack)
+unload v2 (Right v1 : stack) = unload (v1 + v2) stack
