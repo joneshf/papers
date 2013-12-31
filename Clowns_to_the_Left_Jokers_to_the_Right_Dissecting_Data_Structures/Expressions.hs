42eec9193185dabdcce32a4096b9c48906f63630
joneshf <jones3.hardy@gmail.com>
2013-12-31 11:58:14 -0800

    Grossness of the type system.

--- Clowns_to_the_Left_Jokers_to_the_Right_Dissecting_Data_Structures/Expressions.hs
+++ Clowns_to_the_Left_Jokers_to_the_Right_Dissecting_Data_Structures/Expressions.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE KindSignatures, RankNTypes #-}
 module Expressions where
 
 data Expr = Val Int
@@ -27,6 +28,7 @@ data Product1 p q x = Pair1 (p x) (q x)     deriving Show
 type One1 = K1 ()
 
 type Option = Sum1 One1 Id
+
 none :: Option a
 none = L1 (K1 ())
 
@@ -42,3 +44,26 @@ instance (Functor p, Functor q) => Functor (Sum1 p q) where
     fmap f (R1 q) = R1 (fmap f q)
 instance (Functor p, Functor q) => Functor (Product1 p q) where
     fmap f (Pair1 p q) = Pair1 (fmap f p) (fmap f q)
+
+type ExprP = Sum1 (K1 Int) (Product1 Id Id)
+
+-- The types here are too much for me.
+--valP :: Int -> ExprP Int
+valP :: forall (q :: * -> *) a x. a -> Sum1 (K1 a) q x
+valP i = L1 (K1 i)
+
+--addP :: a -> a -> ExprP a
+addP :: forall (p :: * -> *) a. a -> a -> Sum1 p (Product1 Id Id) a
+addP e1 e2 = R1 (Pair1 (Id e1) (Id e2))
+
+data Mu p = In (p (Mu p))
+
+type Expr1 = Mu ExprP
+type AddExpr1 = forall (p :: * -> *). Mu (Sum1 p (Product1 Id Id))
+type ValExpr1 = forall (q :: * -> *) a. a -> Mu (Sum1 (K1 a) q)
+
+val :: ValExpr1
+val i = In (valP i)
+
+add :: AddExpr1 -> AddExpr1 -> AddExpr1
+add e1 e2 = In (addP e1 e2)
