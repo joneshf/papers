c8634084628e2b61aa30d6627e85471201d87752
joneshf <jones3.hardy@gmail.com>
2013-12-31 10:26:56 -0800

    Start Clowns and Jokers.

---
+++ Clowns_to_the_Left_Jokers_to_the_Right_Dissecting_Data_Structures/Expressions.hs
@@ -0,0 +1,19 @@
+module Expressions where
+
+data Expr = Val Int
+          | Add Expr Expr
+    deriving Show
+
+type Stack = [Either Expr Int]
+
+eval :: Expr -> Int
+eval e = load e []
+
+load :: Expr -> Stack -> Int
+load (Val i)    stack  = unload i stack
+load (Add e1 e2) stack = load e1 (Left e2:stack)
+
+unload :: Int -> Stack -> Int
+unload v [] = v
+unload v (Left e:stack) = load e (Right v:stack)
+unload v (Right e:stack) = unload (v + e) stack
